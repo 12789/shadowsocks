cf2ef82bf000bd8991d38fa744076ee678de91ef
Wei Zhang <z12789@gmail.com>
2016-10-30 15:30:40 -0700

    Add user tablename to config

--- shadowsocks/Config.py
+++ shadowsocks/Config.py
@@ -1,12 +1,13 @@
-#Config
+# Config
 MYSQL_HOST = 'mdss.mengsky.net'
 MYSQL_PORT = 3306
 MYSQL_USER = 'ss'
 MYSQL_PASS = 'ss'
 MYSQL_DB = 'shadowsocks'
+MYSQL_USER_TABLE = 'user_nodes'
 
 MANAGE_PASS = 'ss233333333'
-#if you want manage in other server you should set this value to global ip
+# if you want manage in other server you should set this value to global ip
 MANAGE_BIND_IP = '127.0.0.1'
-#make sure this port is idle
+# make sure this port is idle
 MANAGE_PORT = 23333
